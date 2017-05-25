158e9c12be1ed28b8fc56e771d3bedf2704b3b0b
Likai <1328554552@qq.com>
2017-05-25 19:10:24 +0800

    新增后台自动更新天气的功能

--- app/src/main/java/ChooseAreaFragment.java
+++ app/src/main/java/ChooseAreaFragment.java
@@ -99,9 +99,8 @@ public class ChooseAreaFragment extends Fragment {
                     }else if (getActivity() instanceof WeatherActivity){
                         WeatherActivity activity = (WeatherActivity) getActivity();
                         activity.drawerLayout.closeDrawers();
-                        activity.swipeRefreshLayout.setRefreshing(true);
+                        activity.swipeRefresh.setRefreshing(true);
                         activity.requestWeather(weatherId);
-                        
                     }
                 }
             }
